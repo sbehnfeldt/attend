c5196847979b9592bd5b1a2e01760ae3c723904c
Stephen Behnfeldt <stephen@stephenbehnfeldt.com>
2018-09-08 07:55:01 -0400

    Added 'setup.php'. Added base template.

---
+++ src/public/setup.php
@@ -0,0 +1,12 @@
+<?php
+
+require( '../lib/bootstrap.php');
+
+
+
+$loader = new Twig_Loader_Filesystem('../templates');
+$twig = new Twig_Environment($loader, array(
+    'cache' => false
+));
+
+echo $twig->render('setup.html.twig');
