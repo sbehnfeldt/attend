fd361011d54b71102ad02017fb30e4564ad732ce
Stephen Behnfeldt <stephen@stephenbehnfeldt.com>
2018-09-08 07:42:11 -0400

    Moved startup boilerplate to bootstrap.php

--- src/public/index.php
+++ src/public/index.php
@@ -4,20 +4,9 @@
  * Main Script
  ********************************************************************************/
 
-require( '../vendor/autoload.php' );
+require( '../lib/bootstrap.php');
 
-define('INSTALL', dirname(__DIR__));
 
-ini_set( 'error_log', INSTALL . '/logs/php_errors.log' );
-$config = parse_ini_file('../config.ini', true);
-$webroot = $config['app']['root'];
-
-session_save_path( INSTALL . '/sessions');
-session_start( );
-
-ini_set('error_log',  '../logs/php_errors.log');
-$config  = parse_ini_file('../config.ini', true);
-$webroot = $config[ 'app' ][ 'root' ];
 
 $loader = new Twig_Loader_Filesystem('../templates');
 $twig = new Twig_Environment($loader, array(
