20ad647cf0efd12d7c0088fd897a20e1d43abbe5
Stephen Behnfeldt <stephen@stephenbehnfeldt.com>
2018-09-08 07:24:08 -0400

    Simplified directory structure

--- src/public/index.php
+++ src/public/index.php
@@ -1,35 +1,22 @@
 <?php
 
-function my_autoload($className)
-{
-
-    $slash     = "\\";
-    $className = ltrim($className, $slash);
-    $fileName  = '';
-    $namespace = '';
-    if ($lastNsPos = strripos($className, $slash)) {
-        $namespace = substr($className, 0, $lastNsPos);
-        $className = substr($className, $lastNsPos + 1);
-        $fileName  = str_replace($slash, DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
-    }
-    $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
-
-    $require = INSTALL . DIRECTORY_SEPARATOR . 'classes' . DIRECTORY_SEPARATOR . $fileName;
-    require $require;
-}
-
-
 /********************************************************************************
  * Main Script
  ********************************************************************************/
 
 define('INSTALL', dirname(__DIR__));
 
-ini_set('error_log', INSTALL . '/logs/php_errors.log');
+ini_set( 'error_log', INSTALL . '/logs/php_errors.log' );
+$config = parse_ini_file('../config.ini', true);
+$webroot = $config['app']['root'];
+
+session_save_path( INSTALL . '/sessions');
+session_start( );
+
+ini_set('error_log',  '../logs/php_errors.log');
 $config  = parse_ini_file('../config.ini', true);
 $webroot = $config[ 'app' ][ 'root' ];
 
-spl_autoload_register('my_autoload');
 session_save_path(INSTALL . '/sessions');
 session_start();
 ?>
