2c1fd1f13a111dfc12a5c8d90c8a0a419f4a2527
Stephen Behnfeldt <stephen@stephenbehnfeldt.com>
2018-09-09 07:55:07 -0400

    Fetch classrooms from database.

--- src/public/api.php
+++ src/public/api.php
@@ -10,18 +10,31 @@ include '../lib/bootstrap.php';
 $app = new \Slim\App();
 
 $app->get('/api/', function (Request $request, Response $response, array $args) {
-    $response->getBody()->write( 'OK');
+    $response->getBody()->write('OK');
 });
 
 
 $app->get('/api/classrooms', function (Request $request, Response $response, array $args) {
+    $host     = 'localhost';
+    $dbname   = 'attend';
+    $charset  = 'utf8mb4';
+    $user     = 'attend';
+    $password = 'attend';
+
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=$charset", $user, $password, [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
+    $sql = 'SELECT * FROM classrooms';
+    $sth = $pdo->prepare( $sql );
+    $b = $sth->execute();
+    $results = $sth->fetchAll();
+
+
     $response->getBody()->write(json_encode([
-        'data' => [
-            ['id' => 1, 'label' => '123s'],
-            ['id' => 2, 'label' => 'ABC Jrs'],
-            ['id' => 3, 'label' => 'ABCs'],
-        ]
+        'data' => $results
     ]));
+
 
     return $response;
 });
