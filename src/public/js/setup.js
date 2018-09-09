ecb68934b89601a4bf5ae392bd48ac76d592d61e
Stephen Behnfeldt <stephen@stephenbehnfeldt.com>
2018-09-09 06:17:45 -0400

    Added buttons to Classrooms table (placeholder only - not fully functional)

--- src/public/js/setup.js
+++ src/public/js/setup.js
@@ -1,7 +1,37 @@
-;(function(global, $) {
-    $(function() {
-        console.log($('#tabs' ).length );
-        $('#tabs' ).tabs();
-    });
+;(function ( global, $ ) {
 
-})(this, jQuery);
+    $( function () {
+        $( '#tabs' ).tabs();
+
+        var $tab  = $( '#classrooms' );
+        var table = $tab.find( 'table' ).DataTable( {
+            "paging"   : false,
+            "searching": false,
+            "select"   : true
+        } );
+
+        var b0 = new $.fn.dataTable.Buttons( table, {
+            buttons: [ {
+                "text"  : "New",
+                "action": function () {
+                    alert( "New Classroom" );
+                }
+            }, {
+                "extend": "selected",
+                "text"  : "Edit",
+                "action": function () {
+                    alert( "Edit Classroom" );
+                }
+            }, {
+                "extend": "selected",
+                "text"  : "Delete",
+                "action": function () {
+                    alert( "Delete Classroom" );
+                }
+            } ]
+        } );
+        b0.dom.container.eq( 0 ).appendTo( $tab.find( '.run-buttons' ) );
+
+    } );
+
+})( this, jQuery );
