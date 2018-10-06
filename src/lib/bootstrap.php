f8b277b1d93755550dad0bbad214a9adc71bca55
Stephen Behnfeldt <stephen@stephenbehnfeldt.com>
2018-10-06 06:34:25 -0400

    Added php info for dev

--- src/lib/bootstrap.php
+++ src/lib/bootstrap.php
@@ -1,9 +1,9 @@
 <?php
 
-require( '../vendor/autoload.php' );
-
-ini_set( 'error_log',  '../logs/php_errors.log' );
+require('../vendor/autoload.php');
+ini_set('display_errors', 'Off');
+ini_set('error_log', '../logs/php_errors.log');
 $config = parse_ini_file('../config.ini', true);
 
-session_save_path( '../sessions');
-session_start( );
+session_save_path('../sessions');
+session_start();
