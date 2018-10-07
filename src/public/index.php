343bee7c92d828c8f95f3226f51bc4ca1adc6345
Stephen Behnfeldt <stephen@stephenbehnfeldt.com>
2018-10-07 10:42:26 -0400

    1st steps in client-side attendance generation.
    Load classrooms, generate empty table for each classroom.

--- src/public/index.php
+++ src/public/index.php
@@ -6,52 +6,6 @@
 
 require('../lib/bootstrap.php');
 
-$settings = $config[ 'db' ];
-$pdo      = new PDO('mysql:host=' . $settings[ 'host' ] . ';dbname=' . $settings[ 'dbname' ] . ';charset=' . $settings[ 'charset' ],
-    $settings[ 'uname' ], $settings[ 'pword' ], [
-        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-    ]);
-
-
-$repo       = new \Attend\ClassroomsRepository($pdo);
-$classrooms = $repo->select();
-
-usort($classrooms, function ($a, $b) {
-    if ($a[ 'ordering' ] > $b[ 'ordering' ]) {
-        return 1;
-    }
-    if ($a[ 'ordering' ] < $b[ 'ordering' ]) {
-        return -1;
-    }
-
-    return 0;
-});
-
-$repo     = new \Attend\StudentsRepository($pdo);
-$students = [];
-foreach ($classrooms as $i => $c) {
-    $classrooms[ $i ][ 'students' ] = $repo->select([
-        'filters' => 'classroom_id::' . $c[ 'id' ]
-    ]);
-    usort($classrooms[ $i ][ 'students' ], function ($a, $b) {
-        if ($a[ 'family_name' ] > $b[ 'family_name' ]) {
-            return 1;
-        }
-        if ($a[ 'family_name' ] < $b[ 'family_name' ]) {
-            return -1;
-        }
-
-        if ($a[ 'first_name' ] > $b[ 'first_name' ]) {
-            return 1;
-        }
-        if ($a[ 'first_name' ] < $b[ 'first_name' ]) {
-            return -1;
-        }
-
-        return 0;
-    });
-}
 
 $loader = new Twig_Loader_Filesystem('../templates');
 $twig   = new Twig_Environment($loader, array(
@@ -59,5 +13,4 @@ $twig   = new Twig_Environment($loader, array(
 ));
 
 echo $twig->render('index.html.twig', [
-    'classrooms' => $classrooms
 ]);
--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,16 +1,106 @@
 ;(function ( global, $ ) {
     'use strict';
 
+    var Classrooms = (function () {
+        var classrooms = [];
+
+        function load() {
+            $.ajax( {
+                'url'   : 'api/classrooms',
+                'method': 'get',
+
+                'success': function ( json ) {
+                    console.log( json );
+
+                    // Must be expressed as "Classrooms.classrooms", rather than simply referring to "classrooms"
+                    // (a la closure). Not exactly sure why, but "classrooms" alone doesn't work - it refers to
+                    // a separate object somehow.
+                    json.sort( function ( a, b ) {
+                        if ( a.ordering > b.ordering ) return 1;
+                        if ( a.ordering < b.ordering ) return -1;
+                        return 0;
+                    } );
+                    Classrooms.classrooms = json;
+                    AttendanceTab.build();
+                },
+
+                'error': function ( xhr ) {
+                    console.log( xhr );
+                }
+            } )
+        }
+
+        return {
+            'classrooms': classrooms,
+            'load'      : load
+        };
+    })();
+
+
+    var AttendanceTab = (function () {
+        var $tab,
+            $weekOf,
+            $attendance;
+
+        function init( selector ) {
+            $tab    = $( selector );
+            $weekOf = $tab.find( '[name=week-of]' );
+            $weekOf.datepicker();
+            $attendance = $tab.find( '.attendance-page-schedules' );
+        }
+
+        function build() {
+            if ( !Classrooms.classrooms ) {
+                return;
+            }
+            buildAttendanceTables( Classrooms.classrooms );
+        }
+
+        function buildAttendanceTables( classrooms ) {
+            for ( var i = 0; i < classrooms.length; i++ ) {
+                var classroom = classrooms[ i ];
+                $attendance.append( $( '<h3>' ).text( classroom.label ) );
+
+                var $table = $( '<table class="table table-striped table-bordered">' );
+                var $thead = $( '<thead>' );
+                var $tr    = $( '<tr>' );
+                $tr.append( $( '<th>Name</th>' ) );
+                $tr.append( $( '<th>Mon</th>' ) );
+                $tr.append( $( '<th>Tue</th>' ) );
+                $tr.append( $( '<th>Wed</th>' ) );
+                $tr.append( $( '<th>Thu</th>' ) );
+                $tr.append( $( '<th>Fri</th>' ) );
+                $tr.append( $( '<th>Summary</th>' ) );
+                $thead.append( $tr );
+
+                var $tbody = $( '<tbody>' );
+
+                $table.append( $thead );
+                $table.append( $tbody );
+                $attendance.append( $table );
+
+                $table.DataTable( {
+                    'searching': false,
+                    'paging'   : false,
+                    'ordering' : false,
+                    'info'     : false
+                } );
+            }
+
+        }
+
+        return {
+            'init' : init,
+            'build': build
+        };
+    })();
 
     $( function () {
         console.log( "Index page ready" );
         $( '#tabs' ).tabs();
+        AttendanceTab.init( '#attendance-tab' );
+        Classrooms.load();
 
-        $( '.attendance-table' ).DataTable( {
-            'searching': false,
-            'paging'   : false,
-            'ordering' : false
-        } );
     } );
 
 
