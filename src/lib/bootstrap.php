e079e0b02b17b1595881a0314a447e9edb50c969
Stephen <stephen@stephenbehnfeldt.com>
2019-09-12 17:21:03 -0400

    Throw exception if config file cannot be parsed.

--- src/lib/bootstrap.php
+++ src/lib/bootstrap.php
@@ -9,7 +9,10 @@ function bootstrap(string $configFile = '../config.ini')
 
     ini_set('error_log', '../logs/php_errors.log');
 
-    $config = parse_ini_file($configFile, true);
+    if (false == ($config = parse_ini_file($configFile, true))) {
+        throw new Exception(sprintf('Unable to parse config file "%s"', $configFile));
+    }
+
 
     session_save_path('../sessions');
     session_start();
