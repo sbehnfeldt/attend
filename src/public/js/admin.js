a2976154714ebbd6be802de79e35b2a249f89905
Stephen <stephen@stephenbehnfeldt.com>
2020-03-01 17:34:16 -0500

    Encapsulate Accts Table and Acct Dialog into JS classes

--- src/public/js/admin.js
+++ src/public/js/admin.js
@@ -1,58 +1,93 @@
 ;(function (global, $) {
     'use strict';
 
+
+    let AcctsTable = (function () {
+        let $table;
+
+        function init(selector) {
+            $table = $(selector);
+            $table.DataTable({
+                dom: 'rtB',
+                select: {
+                    style: 'single'
+                },
+                buttons: [{
+                    text: 'New',
+                    action: function (e, dt, node, config) {
+                        AcctDlg.open();
+                    }
+                }, {
+                    text: 'Update',
+                    action: function (e, dt, node, config) {
+                        alert("Update")
+                    }
+                }, {
+                    text: 'Delete',
+                    action: function (e, dt, node, config) {
+                        alert("Delete");
+                    }
+                }]
+            });
+        }
+
+        return {
+            init: init
+        };
+    })();
+
+
+    let AcctDlg = (function () {
+        let $dlg;
+
+        function init(selector) {
+            $dlg = $(selector).dialog({
+                modal: true,
+                autoOpen: false,
+                buttons: {
+                    Submit: function () {
+                        console.log($dlg.find('form').serialize());
+                        console.log($dlg.find('form').serializeArray());
+                        $.ajax({
+                            url: 'api/accounts',
+                            method: 'post',
+                            data: $dlg.find('form').serialize(),
+
+                            success: function (success) {
+                                console.log('success');
+                                console.log(success);
+                            },
+                            error: function (xhr) {
+                                console.log('error');
+                                console.log(xhr);
+                            }
+                        });
+                        $dlg.dialog('close');
+                    },
+                    Cancel: function () {
+                        alert("Cancel");
+                        $dlg.dialog('close');
+                    }
+                }
+            });
+        }
+
+        function open() {
+            $dlg.dialog('open');
+        }
+
+        return {
+            init: init,
+            open: open
+        };
+    })();
+
+
     // Document ready handler
     $(function () {
-        let $dlg;
         console.log('Document ready');
-        $('table').DataTable({
-            dom: 'rtB',
-            buttons: [{
-                text: 'New',
-                action: function (e, dt, node, config) {
-                    $dlg.dialog('open');
-                }
-            }, {
-                text: 'Update',
-                action: function (e, dt, node, config) {
-                    alert("Update")
-                }
-            }, {
-                text: 'Delete',
-                action: function (e, dt, node, config) {
-                    alert("Delete");
-                }
-            }]
-        });
-
-        $dlg = $('section#accountPropsDlg').dialog({
-            modal: true,
-            autoOpen: false,
-            buttons: {
-                Submit: function () {
-                    console.log($dlg.find('form').serialize());
-                    console.log($dlg.find('form').serializeArray());
-                    $.ajax({
-                        url: 'api/accounts',
-                        method: 'post',
-                        data: $dlg.find('form').serialize(),
-
-                        success: function (success) {
-                            console.log('success');
-                            console.log(success);
-                        },
-                        error: function (xhr) {
-                            console.log('error');
-                            console.log(xhr);
-                        }
-                    });
-                    $dlg.dialog('close');
-                },
-                Cancel: function () {
-                    alert("Cancel");
-                    $dlg.dialog('close');
-                }
-            }
-        });
     });
+
+    AcctsTable.init('#acctsTable');
+    AcctDlg.init('#accountPropsDlg');
 })(this, jQuery);
