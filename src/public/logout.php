a39a30a731f6c1dff648397552a9151be5b9305d
Stephen <stephen@stephenbehnfeldt.com>
2020-03-15 06:39:28 -0400

    AT-26 Record logouts to security file.

--- src/public/logout.php
+++ src/public/logout.php
@@ -2,13 +2,22 @@
 
 namespace Attend;
 
+use Attend\Database\Account;
 use Attend\Database\TokenQuery;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
 
 require_once '../lib/bootstrap.php';
 $config = bootstrap();
+$logger = new Logger('logouts');
+$logger->pushHandler(new StreamHandler('../logs/security.log'));
 
-if (!empty($_SESSION['account'])) {
+if (empty($_SESSION['account'])) {
+    $logger->warning('Unexpected logout without session');
+} else {
+    /** @var Account $acct */
     $acct = $_SESSION['account'];
+    $logger->info(sprintf('User "%s" logged out', $acct->getUsername()));
 
     // Delete "remember me" tokens when user explicitly logs out
     $tokens = TokenQuery::create()->findByAccountId($acct->getId());
@@ -19,9 +28,9 @@ if (!empty($_SESSION['account'])) {
             die($e->getMessage());
         }
     }
+    unset($_SESSION['account']);
 }
 
-unset($_SESSION['account']);
 setcookie("account_id", null, time() - 1);
 setcookie("token", null, time() - 1);
 session_destroy();
