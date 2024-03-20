1f71c73b3c7d128858687004a4a32d7e2a68a3e8
Stephen <stephen@behnfeldt.pro>
2024-03-20 16:20:19 -0400

    Add "Students" fetch calls to the Attend API

--- public/js/attend-api.js
+++ public/js/attend-api.js
@@ -65,7 +65,69 @@
             }
         }
 
-        return {classrooms};
+        let students = {
+            // Fetch all student data
+            select: async function() {
+                const response = await fetch('/api/students');
+                if (!response.ok) {
+                    // throw new Error(`HTTP error status: ${response.status}`)
+                    console.log(`HTTP error status: ${response.status}`);
+                    return [];
+                }
+                let json = await response.json();
+                return json.data;   // All classroom data
+            },
+
+            // Insert a new student record
+            insert: async function(data) {
+                const response = await fetch('/api/students', {
+                    method: 'POST',
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(data)
+                });
+                if (!response.ok) {
+                    // throw new Error(`HTTP error status: ${response.status}`)
+                    console.log(`HTTP error status: ${response.status}`);
+                    return [];
+                }
+                let json = await response.json();
+                return json.data;   // The new classroom
+            },
+
+            // Update an existing student record
+            update: async function (data) {
+                const response = await fetch(`/api/students/${data.Id}`, {
+                    method: 'PUT',
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(data)
+                });
+                if (!response.ok) {
+                    // throw new Error(`HTTP error status: ${response.status}`)
+                    console.log(`HTTP error status: ${response.status}`);
+                    return [];
+                }
+                let json = await response.json();
+                return json.data;   // The updated classroom
+            },
+
+            // Delete an existing student record
+            remove: async function (id) {
+                const response = await fetch(`/api/students/${id}`, {
+                    method: 'DELETE'
+                });
+                if (!response.ok) {
+                    // throw new Error(`HTTP error status: ${response.status}`)
+                    console.log(`HTTP error status: ${response.status}`);
+                }
+                return;
+            }
+        }
+
+        return {classrooms, students};
     })();
 
 })(this, jQuery);
